7055cf576577baee5475e9a825d191e2f2cccfb7
zheng <noreply>
2023-08-12 19:55:56 -0400

    添加了登录框 添加了MUI库

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {Height} from "@mui/icons-material";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +20,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} style={{height: "100vh", width:"100vw"}}>
+        {/*头部banner*/}
+        <div className={"h-14 w-full bg-white sticky top-0 backdrop-blur bg-opacity-75"}>
+
+        </div>
+
+        {/*body*/}
+        <div className={"h-max w-full"}>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
